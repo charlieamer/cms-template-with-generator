e21c2b2b1f32b64848db0e9cb1c833eeddf0ba97
charlieamer <amer_zavlan@live.com>
2025-03-04 14:12:44 +0100

    feat(sol/generator_specific.cpp): add optional lerp function and comment for makeTest

--- sol/generator_specific.cpp
+++ sol/generator_specific.cpp
@@ -3,6 +3,8 @@
 #include <string>
 #include <assert.h>
 
+// optional function, might be useful if you want to create test cases that
+// gradually increase the difficulty of the generated tests
 float lerp(float a, float b, float f)
 {
     return a + f * (b - a);
@@ -13,6 +15,7 @@ void testInit(int argc, char** argv) {
 }
 
 void makeTest(std::ofstream& inputFile, int subtask, int testcase, float testPercent) {
-    inputFile << rnd.next(1, 100) << " "
-              << rnd.next(1, 100) << std::endl;
+    // You may use rnd.next(...) functions from testlib.h to generate random things.
+    // rnd.next(from, to)
+    // rnd.next(regex_like_pattern)
 }
